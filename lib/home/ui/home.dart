5cff8e2772fa8aa60fd31eaec3b3383e65c4c7e4
ShaunakVaghasia <shaunakvaghasia@gmail.com>
2024-05-23 13:12:52 -0400

    Removed unsued import.

--- lib/home/ui/home.dart
+++ lib/home/ui/home.dart
@@ -1,7 +1,6 @@
 // Created by Shaunak Vaghasia
 
 import 'package:flutter/material.dart';
-import 'package:lifty/auth/core/auth_core_api.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
