7400aa439b93bcb6f3332d7013ddfd907acfbf1f
ShaunakVaghasia <shaunakvaghasia@gmail.com>
2024-05-23 11:51:41 -0400

    Refactored.

--- lib/nav_bar/ui/nav_bar.dart
+++ lib/nav_bar/ui/nav_bar.dart
@@ -1,3 +1,5 @@
+// Created by Shaunak Vaghasia
+
 import 'package:flutter/material.dart';
 
 class NavBar extends StatelessWidget {
@@ -6,6 +8,7 @@ class NavBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return NavigationBar(
+      labelBehavior: NavigationDestinationLabelBehavior.onlyShowSelected,
       destinations: const [
         NavigationDestination(icon: Icon(Icons.fitness_center_rounded), label: 'Workouts'),
         NavigationDestination(icon: Icon(Icons.account_circle_sharp), label: 'Profile'),
