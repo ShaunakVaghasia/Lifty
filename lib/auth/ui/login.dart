4fee2109b6d66ffc01cc70ac351ec1fbbaa303a2
ShaunakVaghasia <shaunakvaghasia@gmail.com>
2024-05-22 20:19:05 -0400

    Added some skeletal code.

---
+++ lib/auth/ui/login.dart
@@ -0,0 +1,12 @@
+// Created by Shaunak Vaghasia
+
+import 'package:flutter/material.dart';
+
+class Login extends StatelessWidget {
+  const Login({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
