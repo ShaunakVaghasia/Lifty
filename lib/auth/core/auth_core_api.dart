5e23014409cf52c9c8358f31c654745639a6c9d5
ShaunakVaghasia <shaunakvaghasia@gmail.com>
2024-05-22 21:38:01 -0400

    Trivial.

--- lib/auth/core/auth_core_api.dart
+++ lib/auth/core/auth_core_api.dart
@@ -1,6 +1,7 @@
 // Created by Shaunak Vaghasia
 
 abstract class AuthCoreApi {
+  /// Callback system to check if user is signed in.
   bool get signedIn;
   void onChangeSignedIn(Function(bool signedIn) callback);
 }
