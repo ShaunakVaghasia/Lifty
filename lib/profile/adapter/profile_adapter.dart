31b36410cbab972a9f0f40e78020064990f3e7bd
ShaunakVaghasia <shaunakvaghasia@gmail.com>
2024-05-23 13:14:11 -0400

    Added Profile skeleton.

--- lib/profile/adapter/profile_adapter.dart
+++ lib/profile/adapter/profile_adapter.dart
@@ -1,1 +1,7 @@
 // Created by Shaunak Vaghasia
+
+import 'package:lifty/profile/core/profile_core_api.dart';
+
+class ProfileAdapter {
+  ProfileAdapter(ProfileCoreApi profileCore) {}
+}
--- lib/profile/core/profile_core.dart
+++ lib/profile/core/profile_core.dart
@@ -1,1 +1,5 @@
 // Created by Shaunak Vaghasia
+
+import 'package:lifty/profile/core/profile_core_api.dart';
+
+class ProfileCore implements ProfileCoreApi {}
