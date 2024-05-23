97e70b57479d8d514dc900814c26da0925e579b0
ShaunakVaghasia <shaunakvaghasia@gmail.com>
2024-05-22 21:51:30 -0400

    Added Auth APIs for firebase users.

--- lib/auth/core/auth_core.dart
+++ lib/auth/core/auth_core.dart
@@ -27,4 +27,36 @@ class AuthCore implements AuthCoreApi {
       }
     });
   }
+
+  @override
+  Future<void> createUserWithEmailAndPassword(String emailAddress, String password) async {
+    try {
+      final credential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: emailAddress,
+        password: password,
+      );
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        print('The password provided is too weak.');
+      } else if (e.code == 'email-already-in-use') {
+        print('The account already exists for that email.');
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  Future<void> signInWithEmailAndPassword(String emailAddress, String password) async {
+    try {
+      final credential =
+          await FirebaseAuth.instance.signInWithEmailAndPassword(email: emailAddress, password: password);
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        print('No user found for that email.');
+      } else if (e.code == 'wrong-password') {
+        print('Wrong password provided for that user.');
+      }
+    }
+  }
 }
--- lib/auth/core/auth_core_api.dart
+++ lib/auth/core/auth_core_api.dart
@@ -4,4 +4,8 @@ abstract class AuthCoreApi {
   /// Callback system to check if user is signed in.
   bool get signedIn;
   void onChangeSignedIn(Function(bool signedIn) callback);
+
+  /// User Authentication and Creation
+  Future<void> createUserWithEmailAndPassword(String emailAddress, String password);
+  Future<void> signInWithEmailAndPassword(String emailAddress, String password);
 }
