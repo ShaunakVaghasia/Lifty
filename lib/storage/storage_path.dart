0c71f7f2d3688ecc84ce443181ef2431fa7bce5a
ShaunakVaghasia <shaunakvaghasia@gmail.com>
2024-05-24 12:08:36 -0400

    Added base paths for collections.

--- lib/storage/storage_path.dart
+++ lib/storage/storage_path.dart
@@ -1,3 +1,21 @@
 // Created by Shaunak Vaghasia
 
-class StoragePath {}
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class StoragePath {
+  static const usersCollection = 'users';
+  static const workoutsCollection = 'workouts';
+  static const nutritionCollection = 'nutrition';
+
+  final base = FirebaseFirestore.instance;
+
+  String? getCurrentUser() => FirebaseAuth.instance.currentUser?.uid;
+
+  // Collections
+  CollectionReference getUserPath() => base.collection(usersCollection);
+  CollectionReference getWorkoutPath() =>
+      base.collection(usersCollection).doc(getCurrentUser()).collection(workoutsCollection);
+  CollectionReference getNutritionPath() =>
+      base.collection(usersCollection).doc(getCurrentUser()).collection(nutritionCollection);
+}
