d71322ea972c74cd16d3fb6133bd5954cde52ead
ShaunakVaghasia <shaunakvaghasia@gmail.com>
2024-05-24 15:30:24 -0400

    Trivial.

--- lib/storage/workout_storage.dart
+++ lib/storage/workout_storage.dart
@@ -4,7 +4,7 @@ import 'package:lifty/storage/storage_path.dart';
 
 class WorkoutStorage implements WorkoutsStorageApi {
   final _pathHelper = StoragePath();
-  Future<List<WorkoutInfo>?> loadAllWorkoutInfo() async {
+  Future<List<WorkoutInfo>?> loadAllWorkouts() async {
     final List<WorkoutInfo> allWorkouts = [];
     try {
       final snapshot = await _pathHelper.workoutPath().get();
