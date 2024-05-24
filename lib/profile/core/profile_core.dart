69be0f914dacaf506bf572af30c1946410d4aea1
Haris <harisrovca@icloud.com>
2024-05-24 15:43:44 -0400

    Initialized ProfileCore

--- lib/profile/core/profile_core.dart
+++ lib/profile/core/profile_core.dart
@@ -2,4 +2,17 @@
 
 import 'package:lifty/profile/core/profile_core_api.dart';
 
-class ProfileCore implements ProfileCoreApi {}
+class ProfileCore implements ProfileCoreApi {
+  final bool _editMode = false;
+  @override
+  bool get editMode => _editMode;
+  @override
+  void onChangeEditMode(Function(bool editMode) callback) =>
+      _onChangeEditMode = callback;
+
+  Function(bool editMode) _onChangeEditMode = (value) {};
+
+  @override
+  Future<void> updateProfile(
+      String firstName, String lastName, String emailAddress) async {}
+}
