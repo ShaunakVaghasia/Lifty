78933a78ad9b171be78b7866631c1744e5a039d7
Haris <harisrovca@icloud.com>
2024-05-24 17:22:27 -0400

    Implemented ProfileStorage in Storage

--- lib/storage/api/storage_api.dart
+++ lib/storage/api/storage_api.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:lifty/storage/api/profile_storage_api.dart';
 import 'package:lifty/storage/api/workouts_storage_api.dart';
 
 @immutable
 abstract class StorageApi {
+  ProfileStorageApi get profile;
   WorkoutsStorageApi get workouts;
 }
--- lib/storage/core/storage.dart
+++ lib/storage/core/storage.dart
@@ -1,14 +1,22 @@
+import 'package:lifty/storage/api/profile_storage_api.dart';
 import 'package:lifty/storage/api/storage_api.dart';
 import 'package:lifty/storage/api/workouts_storage_api.dart';
+import 'package:lifty/storage/core/profile_storage.dart';
 import 'package:lifty/storage/core/workout_storage.dart';
 
 class Storage implements StorageApi {
-  Storage() : _workoutStorage = WorkoutStorage();
+  Storage()
+      : _profileStorage = ProfileStorage(),
+        _workoutStorage = WorkoutStorage();
 
+  final ProfileStorage _profileStorage;
   final WorkoutStorage _workoutStorage;
 
   @override
   WorkoutsStorageApi get workouts => _workoutStorage;
+
+  @override
+  ProfileStorageApi get profile => _profileStorage;
 }
 
 // ignore: prefer_function_declarations_over_variables
