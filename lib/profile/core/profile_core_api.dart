fcde88f8b2f4523b4efc58c165a4a00c450a9f5a
Haris <harisrovca@icloud.com>
2024-05-24 15:43:22 -0400

    Initialized ProfileCoreApi

--- lib/profile/core/profile_core_api.dart
+++ lib/profile/core/profile_core_api.dart
@@ -1,3 +1,11 @@
 // Created by Shaunak Vaghasia
 
-abstract class ProfileCoreApi {}
+abstract class ProfileCoreApi {
+  // Callback to check if profile fields are editable
+  bool get editMode;
+  void onChangeEditMode(Function(bool editMode) callback);
+
+  // Submit profile updates
+  Future<void> updateProfile(
+      String firstName, String lastName, String emailAddress);
+}
