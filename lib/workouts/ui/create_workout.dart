cc05eb31c288fce1a46e83d635750dc2582f2219
ShaunakVaghasia <shaunakvaghasia@gmail.com>
2024-05-25 19:26:41 -0400

    Refactoring.

---
+++ lib/workouts/ui/create_workout.dart
@@ -0,0 +1,132 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class CreateWorkout extends StatelessWidget {
+  CreateWorkout({super.key, required this.getExercises});
+
+  final Function(Map<String, dynamic> exercises) getExercises;
+
+  final TextEditingController exerciseNameControler = TextEditingController();
+  final TextEditingController weightController = TextEditingController();
+  final TextEditingController setsController = TextEditingController();
+  final TextEditingController repsController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      child: SizedBox(
+        height: 200,
+        child: Column(
+          children: [
+            const Padding(padding: EdgeInsets.only(bottom: 10)),
+            TextField(
+              decoration: InputDecoration(
+                filled: true,
+                label: const Text('Exercise Name'),
+                fillColor: Colors.white,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(25),
+                  borderSide: const BorderSide(color: Colors.white, width: 4),
+                ),
+              ),
+              controller: exerciseNameControler,
+            ),
+            const Padding(padding: EdgeInsets.only(bottom: 10)),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  width: 100,
+                  child: TextField(
+                    keyboardType: TextInputType.number,
+                    inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                    decoration: InputDecoration(
+                      filled: true,
+                      label: const Text('Weight'),
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(25),
+                        borderSide: const BorderSide(color: Colors.white, width: 4),
+                      ),
+                    ),
+                    controller: weightController,
+                  ),
+                ),
+                const Padding(padding: EdgeInsets.fromLTRB(5, 0, 5, 0)),
+                SizedBox(
+                  width: 100,
+                  child: TextField(
+                    keyboardType: TextInputType.number,
+                    inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                    decoration: InputDecoration(
+                      filled: true,
+                      label: const Text('Sets'),
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(25),
+                        borderSide: const BorderSide(color: Colors.white, width: 4),
+                      ),
+                    ),
+                    controller: setsController,
+                  ),
+                ),
+                const Padding(padding: EdgeInsets.fromLTRB(5, 0, 5, 0)),
+                SizedBox(
+                  width: 100,
+                  child: TextField(
+                    keyboardType: TextInputType.number,
+                    inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                    decoration: InputDecoration(
+                      filled: true,
+                      label: const Text('Reps'),
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(25),
+                        borderSide: const BorderSide(color: Colors.white, width: 4),
+                      ),
+                    ),
+                    controller: repsController,
+                  ),
+                ),
+              ],
+            ),
+            const Padding(padding: EdgeInsets.only(top: 5)),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    getExercises({
+                      exerciseNameControler.text: [
+                        int.parse(weightController.text),
+                        int.parse(setsController.text),
+                        int.parse(repsController.text),
+                      ]
+                    });
+                    Navigator.pop(context);
+                    // exerciseNameControler.clear();
+                    // weightController.clear();
+                    // setsController.clear();
+                    // repsController.clear();
+                  },
+                  child: const Text('Save'),
+                ),
+                const Padding(padding: EdgeInsets.fromLTRB(10, 0, 10, 0)),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                    // exerciseNameControler.clear();
+                    // weightController.clear();
+                    // setsController.clear();
+                    // repsController.clear();
+                  },
+                  child: const Text('Cancel'),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/workouts/ui/workout_settings.dart
+++ lib/workouts/ui/workout_settings.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:lifty/workouts/core/workouts_core_api.dart';
+import 'package:lifty/workouts/ui/create_workout.dart';
 
 // TODO: Absolute Spaghetti code. Need to change when Mock-ups are done.
 class WorkoutSettings extends StatefulWidget {
@@ -28,142 +29,18 @@ class _WorkoutSettingsState extends State<WorkoutSettings> {
   // Controllers
   late final TextEditingController nameController = TextEditingController(text: widget.name);
 
-  final TextEditingController exerciseNameControler = TextEditingController();
-  final TextEditingController weightController = TextEditingController();
-  final TextEditingController setsController = TextEditingController();
-  final TextEditingController repsController = TextEditingController();
-
   @override
   void dispose() {
     nameController.dispose();
-    weightController.dispose();
-    setsController.dispose();
-    repsController.dispose();
     super.dispose();
   }
 
-  _addExercise() {
-    return showDialog(
-      context: context,
-      builder: (context) {
-        return Dialog(
-          child: SizedBox(
-            height: 200,
-            child: Column(
-              children: [
-                const Padding(padding: EdgeInsets.only(bottom: 10)),
-                TextField(
-                  decoration: InputDecoration(
-                    filled: true,
-                    label: const Text('Exercise Name'),
-                    fillColor: Colors.white,
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(25),
-                      borderSide: const BorderSide(color: Colors.white, width: 4),
-                    ),
-                  ),
-                  controller: exerciseNameControler,
-                ),
-                const Padding(padding: EdgeInsets.only(bottom: 10)),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    SizedBox(
-                      width: 100,
-                      child: TextField(
-                        keyboardType: TextInputType.number,
-                        inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-                        decoration: InputDecoration(
-                          filled: true,
-                          label: const Text('Weight'),
-                          fillColor: Colors.white,
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(25),
-                            borderSide: const BorderSide(color: Colors.white, width: 4),
-                          ),
-                        ),
-                        controller: weightController,
-                      ),
-                    ),
-                    const Padding(padding: EdgeInsets.fromLTRB(5, 0, 5, 0)),
-                    SizedBox(
-                      width: 100,
-                      child: TextField(
-                        keyboardType: TextInputType.number,
-                        inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-                        decoration: InputDecoration(
-                          filled: true,
-                          label: const Text('Sets'),
-                          fillColor: Colors.white,
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(25),
-                            borderSide: const BorderSide(color: Colors.white, width: 4),
-                          ),
-                        ),
-                        controller: setsController,
-                      ),
-                    ),
-                    const Padding(padding: EdgeInsets.fromLTRB(5, 0, 5, 0)),
-                    SizedBox(
-                      width: 100,
-                      child: TextField(
-                        keyboardType: TextInputType.number,
-                        inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-                        decoration: InputDecoration(
-                          filled: true,
-                          label: const Text('Reps'),
-                          fillColor: Colors.white,
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(25),
-                            borderSide: const BorderSide(color: Colors.white, width: 4),
-                          ),
-                        ),
-                        controller: repsController,
-                      ),
-                    ),
-                  ],
-                ),
-                const Padding(padding: EdgeInsets.only(top: 5)),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    ElevatedButton(
-                      onPressed: () {
-                        exercises.addAll({
-                          exerciseNameControler.text: [
-                            int.parse(weightController.text),
-                            int.parse(setsController.text),
-                            int.parse(repsController.text),
-                          ]
-                        });
-                        Navigator.pop(context);
-                        exerciseNameControler.clear();
-                        weightController.clear();
-                        setsController.clear();
-                        repsController.clear();
-                      },
-                      child: const Text('Save'),
-                    ),
-                    const Padding(padding: EdgeInsets.fromLTRB(10, 0, 10, 0)),
-                    ElevatedButton(
-                      onPressed: () {
-                        Navigator.pop(context);
-                        exerciseNameControler.clear();
-                        weightController.clear();
-                        setsController.clear();
-                        repsController.clear();
-                      },
-                      child: const Text('Cancel'),
-                    ),
-                  ],
-                )
-              ],
-            ),
-          ),
-        );
-      },
-    );
-  }
+  _addExercise() => showDialog(
+        context: context,
+        builder: (context) => CreateWorkout(
+          getExercises: (exercise) => exercises.addAll(exercise),
+        ),
+      );
 
   @override
   Widget build(BuildContext context) {
