6e06270972949ac8c18c2355156cc591bbe16c05
ShaunakVaghasia <shaunakvaghasia@gmail.com>
2024-05-24 22:44:05 -0400

    WIP: Workout Settings.

--- lib/workouts/ui/workout_settings.dart
+++ lib/workouts/ui/workout_settings.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class WorkoutSettings extends StatefulWidget {
   const WorkoutSettings({super.key});
@@ -8,12 +9,109 @@ class WorkoutSettings extends StatefulWidget {
 }
 
 class _WorkoutSettingsState extends State<WorkoutSettings> {
+  final Map<String, List> exercises = {};
+
+  // Controllers
   final TextEditingController nameController = TextEditingController();
 
   @override
   void dispose() {
     nameController.dispose();
+
     super.dispose();
+  }
+
+  _addExercise() {
+    final TextEditingController exerciseNameControler = TextEditingController();
+    final TextEditingController setsController = TextEditingController();
+    final TextEditingController repsController = TextEditingController();
+    return showDialog(
+      context: context,
+      builder: (context) {
+        return Dialog(
+          child: SizedBox(
+            height: 200,
+            child: Column(
+              children: [
+                const Padding(padding: EdgeInsets.only(bottom: 10)),
+                TextField(
+                  decoration: InputDecoration(
+                    filled: true,
+                    label: const Text('Exercise Name'),
+                    fillColor: Colors.white,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(25),
+                      borderSide: const BorderSide(color: Colors.white, width: 4),
+                    ),
+                  ),
+                  controller: exerciseNameControler,
+                ),
+                const Padding(padding: EdgeInsets.only(bottom: 10)),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      width: 150,
+                      child: TextField(
+                        keyboardType: TextInputType.number,
+                        inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                        decoration: InputDecoration(
+                          filled: true,
+                          label: const Text('Sets'),
+                          fillColor: Colors.white,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(25),
+                            borderSide: const BorderSide(color: Colors.white, width: 4),
+                          ),
+                        ),
+                        controller: setsController,
+                      ),
+                    ),
+                    const Padding(padding: EdgeInsets.fromLTRB(10, 0, 10, 0)),
+                    SizedBox(
+                      width: 150,
+                      child: TextField(
+                        keyboardType: TextInputType.number,
+                        inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                        decoration: InputDecoration(
+                          filled: true,
+                          label: const Text('Reps'),
+                          fillColor: Colors.white,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(25),
+                            borderSide: const BorderSide(color: Colors.white, width: 4),
+                          ),
+                        ),
+                        controller: repsController,
+                      ),
+                    ),
+                  ],
+                ),
+                const Padding(padding: EdgeInsets.only(top: 5)),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
+                        exercises.addAll({exerciseNameControler.text: []});
+                      },
+                      child: const Text('Save'),
+                    ),
+                    const Padding(padding: EdgeInsets.fromLTRB(10, 0, 10, 0)),
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      child: const Text('Cancel'),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+        );
+      },
+    );
   }
 
   @override
@@ -50,55 +148,22 @@ class _WorkoutSettingsState extends State<WorkoutSettings> {
               child: Container(
                 height: MediaQuery.of(context).size.height * 0.55,
                 color: Colors.red,
-                child: Column(
+                child: Stack(
                   children: [
-                    const Padding(padding: EdgeInsets.only(bottom: 10)),
-                    TextField(
-                      decoration: InputDecoration(
-                        filled: true,
-                        label: const Text('Exercise Name'),
-                        fillColor: Colors.white,
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(25),
-                          borderSide: const BorderSide(color: Colors.white, width: 4),
-                        ),
-                      ),
-                      controller: nameController,
+                    ListView.builder(
+                      itemCount: exercises.length,
+                      itemBuilder: (context, index) {
+                        return const Text('1');
+                      },
                     ),
-                    const Padding(padding: EdgeInsets.only(bottom: 10)),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        SizedBox(
-                          width: 150,
-                          child: TextField(
-                            decoration: InputDecoration(
-                              filled: true,
-                              label: const Text('Sets'),
-                              fillColor: Colors.white,
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(25),
-                                borderSide: const BorderSide(color: Colors.white, width: 4),
-                              ),
-                            ),
-                          ),
-                        ),
-                        const Padding(padding: EdgeInsets.fromLTRB(10, 0, 10, 0)),
-                        SizedBox(
-                          width: 150,
-                          child: TextField(
-                            decoration: InputDecoration(
-                              filled: true,
-                              label: const Text('Reps'),
-                              fillColor: Colors.white,
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(25),
-                                borderSide: const BorderSide(color: Colors.white, width: 4),
-                              ),
-                            ),
-                          ),
-                        ),
-                      ],
+                    Align(
+                      alignment: Alignment.bottomCenter,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          _addExercise();
+                        },
+                        child: const Text('Add Exercise'),
+                      ),
                     ),
                   ],
                 ),
